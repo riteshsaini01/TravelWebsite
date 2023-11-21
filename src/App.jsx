8561446e4c290e5d5a27c71f711f0fb544ddbe95
riteshsaini01 <b210885@skit.ac.in>
2023-11-21 17:50:08 +0530

    Adding taskbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import "./App.css"
-import Navbar from "./Components/Navbar/Navbar"
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home"
+import Destinations from "./Components/Destinations/Destination"
+import Footer from "./Components/Footer/Footer"
+import Middle from "./Components/Middle/Middle"
+import Portfolio from "./Components/Portfolio/Portfolio"
+import Questions from "./Components/Questions/Questions"
+import Reviews from "./Components/Reviews/Reviews"
+import Subscribe from "./Components/Reviews/Reviews"
+
 
 function App() {
   
 
   return (
    <div>
-    <Navbar/> </div>
+    <Navbar/>
+    {/*<Home/>
+    <Middle/>
+    <Destinations/>
+    <Portfolio/>
+    <Reviews/>
+    <Questions/>
+    <Subscribe/>
+  <Footer/>*/}
+     </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 import "./Navbar.css";
+
+//imported icons
+import { BiLogoMediumOld } from "react-icons/bi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { PiDotsNineBold } from "react-icons/pi";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navBar">
+      <div className="logoDiv">
+      <BiLogoMediumOld className='icon'/>
+      <span>OU-Trips</span>
+      </div>
+
+        <div className="menu">
+          <ul>
+            <li className="navList">Destination</li>
+            <li className="navList">Aboutus</li>
+            <li className="navList">Testimonial</li>
+            <li className="navList">Gallery</li>
+          </ul>
+
+          {/*Icon to remove NavBar */}
+          <AiFillCloseCircle className='icon' />
+        </div>
+        <button className="signInBin btn">
+          Sign Up
+        </button>
+        {/*Icon to Toggle NavBar */}
+        <PiDotsNineBold className='icon' />
+      </div>
+      
+
+    
   )
 }
 
