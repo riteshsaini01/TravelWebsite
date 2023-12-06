eaa846726e51960d0165def8adc9da62aa8ea891
Madanlal Bidiyasar <madanlalbidiyasar@madanlalbidiyasar.local>
2023-12-06 21:53:11 +0530

    Adding a new DestinationDB file which will contain data of all the destinations and changed destination.jsx to make search functionable and these are committed to new branch 'v1'

---
+++ src/Components/Destinations/DestinationDB.js
@@ -0,0 +1,62 @@
+// Imported images
+import image1 from "../../Assets/image1.jpg";
+import image2 from "../../Assets/image2.jpg";
+import image3 from "../../Assets/image3.jpg";
+import image4 from "../../Assets/image4.jpg";
+import image5 from "../../Assets/image5.jpg";
+import image6 from "../../Assets/image6.jpg";
+import image7 from "../../Assets/image7.jpg";
+
+const DestinationDB = [
+  {
+    id: 1,
+    img: image1,
+    name: "Jaipur",
+    location: "Rajasthan",
+    rating: 4.7,
+  },
+  {
+    id: 2,
+    img: image2,
+    name: "Udaipur",
+    location: "Rajasthan",
+    rating: 4.2,
+  },
+  {
+    id: 3,
+    img: image3,
+    name: "Jaisalmer",
+    location: "Rajasthan",
+    rating: 4.3,
+  },
+  {
+    id: 4,
+    img: image4,
+    name: "Ooty",
+    location: "TamilNadu",
+    rating: 4.8,
+  },
+  {
+    id: 5,
+    img: image5,
+    name: "Manali",
+    location: "Himachal Pradesh",
+    rating: 4.1,
+  },
+  {
+    id: 6,
+    img: image6,
+    name: "Panaji",
+    location: "Goa",
+    rating: 4.5,
+  },
+  {
+    id: 7,
+    img: image7,
+    name: "Bridge",
+    location: "Mumbai",
+    rating: 4.3,
+  },
+];
+
+export default DestinationDB;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,53 +1,53 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 import "./Home.scss";
 
 //Imported Assets
-import Video from '../../Assets/video.mp4';
-import image1 from '../../Assets/image1.jpg';
-import image2 from '../../Assets/image2.jpg';
-import image3 from '../../Assets/image3.jpg';
-import image4 from '../../Assets/image4.jpg';
+import Video from "../../Assets/video.mp4";
+import image1 from "../../Assets/image1.jpg";
+import image2 from "../../Assets/image2.jpg";
+import image3 from "../../Assets/image3.jpg";
+import image4 from "../../Assets/image4.jpg";
 
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 //Imported icons
-import {FaArrowRight} from 'react-icons/fa'
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
-  useEffect(()=>{
-    Aos.init({duration: 2000})
-  },[]);
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <div className='Home'>
-   <div className="videoBg">
-    <video src={Video} autoPlay loop muted></video>/  </div>
-
-  <div className="sectionText">
-    <h1 data-aos='fade-up'>Unlock Your Travel Dreams With us!</h1>
-    <p data-aos='fade-up'>
-      Discover the world's most adventurous nature,
-      life is so short for a trip.
-    </p>
-    <button className='btn flex' data-aos='fade-up'>Get Started<FaArrowRight className="icon">
-      </FaArrowRight>
-      </button>
-  </div>
-
-  <div className="popularPlaces">
-    <div className="content">
-      <h3 data-aos='fade-up'>Popular Places</h3>
-      <div className='images flex' data-aos='fade-up'>
-        <img src= {image1} alt="Destination Images"/>
-        <img src= {image2} alt="Destination Images"/>
-        <img src= {image3} alt="Destination Images"/>
-        <img src= {image4} alt="Destination Images"/>
+    <div id="home" className="Home">
+      <div className="videoBg">
+        <video src={Video} autoPlay loop muted></video>/{" "}
       </div>
 
-    </div>
-  </div>
-</div>
-  )
-}
+      <div className="sectionText">
+        <h1 data-aos="fade-up">Unlock Your Travel Dreams With us!</h1>
+        <p data-aos="fade-up">
+          Discover the world's most adventurous nature, life is so short for a
+          trip.
+        </p>
+        <button className="btn flex" data-aos="fade-up">
+          Get Started<FaArrowRight className="icon"></FaArrowRight>
+        </button>
+      </div>
 
-export default Home
+      <div className="popularPlaces">
+        <div className="content">
+          <h3 data-aos="fade-up">Popular Places</h3>
+          <div className="images flex" data-aos="fade-up">
+            <img src={image1} alt="Destination Images" />
+            <img src={image2} alt="Destination Images" />
+            <img src={image3} alt="Destination Images" />
+            <img src={image4} alt="Destination Images" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
