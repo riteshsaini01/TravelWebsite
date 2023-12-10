c10fb70c09f0a8d061a1e86b5f8aea03e6e4a11b
Ritesh Saini <99114592+riteshsaini01@users.noreply.github.com>
2023-12-10 10:31:32 +0000

    Added progressbar

--- src/Components/Destinations/DestinationCardDetails.jsx
+++ src/Components/Destinations/DestinationCardDetails.jsx
@@ -9,6 +9,18 @@ import video1 from "../../Assets/jaipur.mp4";
 function DestinationCardDetails() {
   const { id } = useParams();
 
+  /**{let number = document.getElementById("number");
+  let counter = 0;
+  setInterval(() => {
+    if(counter == 65){
+      clearInterval();
+    }
+    else{
+      counter += 1;
+      number.innerHTML = counter + "%";
+    }
+  }, 30);}**/
+
   return (
     <div>
       <Navbar />
@@ -32,8 +44,272 @@ function DestinationCardDetails() {
 
 
       <div className="details">
+    
+        <div className="progressbar ">
+          <ul>
+            <li className="detail1">
+              <h2>Ranking:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> #3 </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+            </div>
+          </li>
 
-        <p>hello</p>
+
+          <li className="detail2">
+            <h2>Cost for Nomad:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 600$  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+
+          <li>
+          <h2>WiFi:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 20Mbps  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Fun:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 3/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Air Quality(AQI):</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 120  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Safety:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 4/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Electricity:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 4/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>NightLife:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 5/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Place for WFH:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 5/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>AVG Trip Time:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 4days  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Safe for Women:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 4/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Climate:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> Warm  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+
+          <li>
+          <h2>Medical:</h2>
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                <div id="number"> 5/5  </div>
+              </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+            </defs>
+            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+           </svg>
+           </div>
+          </li>
+          </ul>
+        </div>
         </div>
       </div> 
     
